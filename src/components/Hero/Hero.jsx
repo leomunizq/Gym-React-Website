6298304c3c9a14bcfe194640d30f26e9b5eb06fc
leomunizq <leonardo.mdq@hotmail.com>
2022-11-13 23:07:20 +0100

    Header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,6 +6,23 @@ function Hero() {
     <div className="hero">
       <div className="left-h">
         <Header />
+
+        {/* Button Adversiting */}
+        <div className="button-ad">
+          <div></div>
+          <span>Lorem ipsum, dolor sit amet consectetur adipisi</span>
+        </div>
+        {/* Hero Heading */}
+
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal Body</span>
+          </div>
+        </div>
       </div>
       <div className="right-h">right</div>
     </div>
